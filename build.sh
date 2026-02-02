e2c164940d231e6ea09bd466ca0763f81a911f2c
js6699415 <js6699415@gmail.com>
2026-02-02 12:20:13 -0500

    Fix build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 # --- LIMPIEZA DE LOGS ACTIVADA ---
